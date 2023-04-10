36752f6e3b790eb85302affe098297c7a153ec4b
mordobiv <mordobiv@gmail.com>
2023-04-10 12:18:36 +0200

    feat: add tests

--- vite-project/src/components/card-item/card-item.test.tsx
+++ vite-project/src/components/card-item/card-item.test.tsx
@@ -5,7 +5,6 @@ import CardItem from './index';
 describe('Card item', () => {
   it('renders cart item', () => {
     render(<CardItem label="testLabel" data="testData" />);
-    expect(screen.getByText('testLabel')).toBeInTheDocument();
     expect(screen.getByText('testData')).toBeInTheDocument();
   });
 });
---
+++ vite-project/src/components/modal/modal.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Modal from './modal';
+
+describe('Modal', () => {
+  it('renders modal', () => {
+    const data = {
+      id: 1,
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human',
+      type: '',
+      gender: 'Male',
+      origin: { name: 'Earth (C-137)', url: 'https://rickandmortyapi.com/api/location/1' },
+      location: { name: 'Citadel of Ricks', url: 'https://rickandmortyapi.com/api/location/3' },
+      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      episode: [
+        'https://rickandmortyapi.com/api/episode/1',
+        'https://rickandmortyapi.com/api/episode/2',
+      ],
+      url: 'https://rickandmortyapi.com/api/character/1',
+      created: '2017-11-04T18:48:46.250Z',
+    };
+    render(<Modal handleClose={() => 1} data={data} />);
+    expect(screen.getByText(/Citadel/i)).toBeInTheDocument();
+  });
+});
