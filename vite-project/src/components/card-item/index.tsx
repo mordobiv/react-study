6d70e24d814fe7565edf1bec6da5d11100bb3cc3
mordobiv <mordobiv@gmail.com>
2023-04-10 11:49:42 +0200

    fix: ts errors

--- vite-project/src/components/card-item/index.tsx
+++ vite-project/src/components/card-item/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import styles from './card-item.module.scss';
 
-export default function CardItem({ label, data }) {
+export default function CardItem({
+  label,
+  data,
+}: {
+  label: string | number;
+  data: string | number;
+}) {
   return (
     <div>
       <span className={styles.modal_label}>{label}: </span>
--- vite-project/src/components/modal/modal.tsx
+++ vite-project/src/components/modal/modal.tsx
@@ -12,13 +12,13 @@ function Modal({ handleClose, data }: { handleClose: () => void; data: NodeType 
           <CardItem label={'name'} data={data.name} />
           <CardItem label={'status'} data={data.status} />
           <CardItem label={'species'} data={data.species} />
-          <CardItem label={'type'} data={data.type} />
+          <CardItem label={'type'} data={data.type || ''} />
           <CardItem label={'gender'} data={data.gender} />
           {data.origin && <CardItem label={'origin'} data={data.origin.name} />}
           {data.location && <CardItem label={'location'} data={data.location.name} />}
           <CardItem label={'url'} data={data.url} />
           <CardItem label={'created'} data={data.created} />
-          <CardItem label={'episodes'} data={data.episode.length} />
+          <CardItem label={'episodes'} data={data.episode.length || 0} />
         </div>
         <Img src={data.image} className={'images_photo'} />
       </div>
