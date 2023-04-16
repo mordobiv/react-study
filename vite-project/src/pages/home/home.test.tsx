79e42314d5b6facd4ff6363ffeacc97822238ccd
mordobiv <mordobiv@gmail.com>
2023-04-16 22:18:16 +0300

    feat: init redux

--- vite-project/src/pages/home/home.test.tsx
+++ vite-project/src/pages/home/home.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Home from './Home';
 
 describe('Home', () => {
   it("renders home page's elements", () => {
     render(<Home />);
     expect(screen.getByPlaceholderText(/search/i)).toBeInTheDocument();
+  });
+
+  it('apply filter', () => {
+    render(<Home />);
+    const search = screen.getByPlaceholderText(/search/i);
+    fireEvent.change(search, { target: { value: 'Rick' } });
+    fireEvent.keyPress(search, 'Enter');
+    expect(screen.getByText(/Rick/i)).toBeInTheDocument();
   });
 });
